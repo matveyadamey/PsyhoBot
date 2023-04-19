5e7bb18188a53aa36e12b1b505f7a347151ada3d
matveyadamey <71337113+matveyadamey@users.noreply.github.com>
2023-04-19 18:07:13 +0300

    бебра

--- bot.py
+++ bot.py
@@ -25,14 +25,16 @@ def start(message):
     #get username
     global username
     username=str(message.chat.id)
-    c.push(username,[currentQuestion,ISum])
 
-    bot.send_message(message.chat.id,"Привет! Я бот-психолог, я хочу помочь тебе решить все твои проблемы и разобраться в себе.")
+    bot.send_message(message.chat.id,"Привет, "+message.from_user.first_name+
+                     "! Я бот-психолог, я хочу помочь тебе решить все твои проблемы и разобраться в себе.")
     bot.send_message(message.chat.id,"Для начала пройди небольшой тест."
-                                     "На все вопросы выводите число от 0 до 6, где 0 означает, что изложенная позиция Вам совершенно не подходит,а 6 что Вы полностью согласны с вышеуказанным")
+                                     "На все вопросы отвечай числом от 0 до 6, где 0 означает, что изложенная позиция тебе совершенно не подходит,"
+                                     "а 6 что ты полностью согласен с вышеуказанным")
 
     #отправляем текущий вопрос
-    bot.send_message(message.chat.id,t[c.put(str(message.chat.id))[0]])
+    bot.send_message(message.chat.id,t[0])
+    c.push(username, [currentQuestion+1, ISum])
 
 
 #принимаем ответ
@@ -40,30 +42,29 @@ def start(message):
 def lalal(message):
     global ISum
     global currentQuestion
-
+    currentQuestion=c.put(str(message.chat.id))[0]
+    ISum=c.put(str(message.chat.id))[1]
     #если не все вопросы заданы
-    if(c.put(str(message.chat.id))[0]<=len(t)-2):
+    if(currentQuestion<=len(t)-2):
         #проверяем является ли ответ числом
         try:
             text=int(message.text)
-            if (text):
-                    #если да, тогда смотрим входит ли он в диапозон от 0 до 6
-                    if (0<=text<= 6):
-                        #увеличиваем результат и счетчик вопросов
-                        print(c.put(str(message.chat.id))[0],c.put(str(message.chat.id))[1])
-                        c.push(str(message.chat.id),[c.put(str(message.chat.id))[0],c.put(str(message.chat.id))[1]+text])
-                        print(c.put(str(message.chat.id))[0], c.put(str(message.chat.id))[1])
-                        c.push(str(message.chat.id),[c.put(str(message.chat.id))[0]+1,c.put(str(message.chat.id))[1]])
-                        bot.send_message(message.chat.id, t[c.put(str(message.chat.id))[0]])
-                    #если не входит повторяем текущий вопрос
+            #если да, тогда смотрим входит ли он в диапозон от 0 до 6
+            if (0<=text<= 6):
+                #увеличиваем результат и счетчик вопросов
+                ISum+=text
+                c.push(str(message.chat.id),[currentQuestion,ISum])
+                c.push(str(message.chat.id),[currentQuestion+1,ISum])
+                bot.send_message(message.chat.id, t[currentQuestion])
+            #если не входит повторяем текущий вопрос
             else:
-                        bot.send_message(message.chat.id, "шкала от 0 до 6")
-                        bot.send_message(message.chat.id, t[c.put(str(message.chat.id)[0])])
+                bot.send_message(message.chat.id, "шкала от 0 до 6")
+                bot.send_message(message.chat.id, t[currentQuestion-1])
 
         except:
             #если ответ не является числом, говорим об этом юзеру
             bot.send_message(message.chat.id, "Я тебя не понимаю")
-            bot.send_message(message.chat.id, t[c.put(str(message.chat.id))[0]])
+            bot.send_message(message.chat.id, t[currentQuestion-1])
 
 
     else:
@@ -71,6 +72,8 @@ def lalal(message):
         diagramGenerator.Diagram().bebra([ISum, 100 - ISum])
         photo=open("bebra.png","rb")
         bot.send_photo(message.chat.id,photo)
+
+
         bot.send_message(message.chat.id,"Совет: если вы будете увереннее в себе, то сможете достичь большего")
 
         #обнуляем текущий вопрос и удаляем тест из списка ожидающих прохождение
